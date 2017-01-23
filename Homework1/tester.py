8c8620cb582ccdaf684104faaea313c052e30f6e
YF-N <Yunfan.Niu@outlook.com>
2017-01-23 15:18:41 -0600

    Finished the tester class

--- Homework1/tester.py
+++ Homework1/tester.py
@@ -1,4 +1,4 @@
-class tester(object):
+class tester():
     def __init__(self):
         print("Welcome to the test!\n")
         first=input("Please enter your first name >>>  ")
@@ -9,10 +9,39 @@ class tester(object):
         self.lName=last
         self.ID=id
 
+        self.testLog=[]
+        self.answeredLog=[]
+
+        self._score=0
+
     def info(self):
         return 'Tester name is: {} {}  Tester ID is: {}'.format(self.fName,self.lName,self.ID)
 
+    def addLog(self,res=True,index=0):
+        if res==True:
+            self.testLog.append('{}:√'.format(index))
+            self._score+=1
+        else:
+            self.testLog.append('{}:¢'.format(index))
+        self.answeredLog.append(index)
+        return res
 
+    def isAnswered(self,index=0):
+        if index in self.answeredLog:
+            return True
+        else:
+            return  False
 
+    def getScore(self):
+        return self._score
+
+    def recordLog(self):
+        testHistory=open('testHistory.txt','a')
+        testHistory.write('{0:10}|{1:15}|{2:15}|{3:50}|{4:5}\n'.format(self.ID,self.fName,self.lName,self.testLog,self._score))
+        testHistory.close()
+        return True
+
+    def goodbye(self):
+        print("Thank you, test is finished. Your score is:{}/10".format(self._score))
 
 
