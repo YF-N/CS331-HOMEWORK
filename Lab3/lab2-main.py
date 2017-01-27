c0110044d9bba9c322d9ce058a313c0e122eaaca
YF-N <Yunfan.Niu@outlook.com>
2017-01-26 20:52:20 -0600

    finished the two sort function and tested

--- Lab3/lab2-main.py
+++ Lab3/lab2-main.py
@@ -15,4 +15,17 @@ def descending_bubble(alist=[]):
 
 
 testList = [33, 45, 67, 89, 23, 42, 62, 1, 85, 26]
-print(descending_bubble(testList))
+#print(descending_bubble(testList))
+
+def descending_insertion(alist=[]):
+    for sorted in range(1,len(alist)):
+        for sorting in range(sorted,0,-1):
+            if alist[sorting]>alist[sorting-1]:        #current position is not good for the one is sorting
+                temp = alist[sorting]
+                alist[sorting] = alist[sorting - 1]
+                alist[sorting - 1] = temp
+            else:                                       #current position is good for the one is sorting
+                break                                   #go to the next item of outer loop
+    return alist
+
+#print(descending_insertion(testList))
